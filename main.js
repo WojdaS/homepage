d81cfe8496895995ae09a423dcca0beb96a07649
Slawomir Wojda <slwojda@poczta.onet.pl>
2019-06-28 22:09:56 +0200

    change navigation to a list

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ let abcd = document.querySelector(".about__paragraph-js");
 console.log(abcd.innerHTML);
 
 abcd.innerHTML = "Podmiana treści przez JavaScript";
+
+if ("javascript" != "java") {
+  console.log("to prawdza");
+}
