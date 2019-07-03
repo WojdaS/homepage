6dadbcd5a56277a9334a21cef9534f469745ad4e
Slawomir Wojda <slwojda@poczta.onet.pl>
2019-07-03 18:25:49 +0200

    homework/ created welcome function

--- main.js
+++ main.js
@@ -7,3 +7,13 @@ abcd.innerHTML = "Podmiana treści przez JavaScript";
 if ("javascript" != "java") {
   console.log("to prawdza");
 }
+
+let name = prompt("Podaj swoje imię:");
+let age = prompt("Podaj ile masz lat:");
+
+function welcome(name, age) {
+  let hello = document.querySelector(".header__title");
+  hello.innerHTML = ('Cześć ' + name + ' masz ' + age);
+}
+
+welcome(name, age);
